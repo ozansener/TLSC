8764f573b4de765a9903bf20e6bbff877d6b5bb6
Ozan Sener <ozansener@gmail.com>
2011-04-16 03:14:13 +0300

    Much more OOP,only XML I/O and decision functions are missing

--- lang.py
+++ lang.py
@@ -1,85 +1,6 @@
-# -*- coding: utf-8 -*-
-from rss import *
-from pylab import *
 import ngram
-
-global letterList
-sList = {} # içerisine nGram şeyleri doldurulacak. her dilinki ayrı ayrı tabi.
-
-# Conversion of some turkish language specific characters
-charList = {"\x80":"c",
-            "\x8e":"a",
-            "\x99":"o",
-            "\xa3":"u",
-            chr(0xe2):"o", # 0xe2 karakterini silemiyorum!!! :S
-            "ğ":"g",
-            "ü":"u",
-            "ş":"s",
-            "ı":"i",
-            "ö":"o",
-            "ç":"c",
-            "Ğ":"G",
-            "Ü":"U",
-            "Ş":"S",
-            "İ":"I",
-            "Ö":"O",
-            "Ç":"C",
-            "!":"",
-            ",":"",
-            ".":"",
-            ";":"",
-            ":":"",
-            ")":"",
-            "(":"",
-            "'":"",
-
-            '"':"",
-            "1":"",
-            "2":"",
-            "3":"",
-            "4":"",
-            "5":"",
-            "6":"",
-            "7":"",
-            "8":"",
-            "9":"",
-            "0":"",
-            "-":"",
-            "_":"",
-            "?":"",
-            "%":"",
-            "$":"",
-            "&":"",
-            "/":"",
-            "\\":""} #This set is not complete!!
-
-feedList = ["cnn.xml","bbc.xml"] #If new feeds want to be precessed, can be added to here
-
-#Replace language specfiic characters
-def repChar(inputStr,charDict):
-    ret = inputStr.encode("utf-8")
-    for x in charDict:
-        ret = ret.replace(x,charDict[x])
-    return ret
-
-kedi = "the cat sat on the mat! so i shouted at him!!!"
-
-kedi = repChar(kedi,charList)
-print kedi.split()
-
-n=ngram.ngram()
-def processFeeds():
-    for x in feedList:
-        feed = getFeed(x)
-        for channel in feed:
-            print "Feed: %s, %s" %(channel["title"],channel["desc"])
-            print "Language: %s" %(channel["lang"])
-            for obj in channel["titles"]:
-                for x in repChar(obj["title"],charList).lower().split():
-                    n.update(x,channel["lang"])
-                for x in repChar(obj["desc"],charList).lower().split():
-                    n.update(x,channel["lang"])
-
+import process_rss
+from pylab import *
 
 
 def plotHistograms(values):
@@ -94,8 +15,13 @@ def plotHistograms(values):
 	plot(xAxis,array) 	
 	print array
 	
-processFeeds()
 x=1
+
+r=process_rss.process_rss()
+r.add_rss("cnn.xml")
+r.add_rss("bbc.xml")
+r.process()
+n=r.getNgram()
 a=n.getsList();
 for i in a:
 	subplot(2,1,x)
---
+++ process_rss.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+from rss import *
+import ngram
+
+class process_rss(object):
+	def __init__(self):
+		# Conversion of some turkish language specific characters
+		self.charList = {"\x80":"c",
+		    "\x8e":"a",
+		    "\x99":"o",
+		    "\xa3":"u",
+		    chr(0xe2):"o", # 0xe2 karakterini silemiyorum!!! :S
+		    "ğ":"g",
+		    "ü":"u",
+		    "ş":"s",
+		    "ı":"i",
+		    "ö":"o",
+		    "ç":"c",
+		    "Ğ":"G",
+		    "Ü":"U",
+		    "Ş":"S",
+		    "İ":"I",
+		    "Ö":"O",
+		    "Ç":"C",
+		    "!":"",
+		    ",":"",
+		    ".":"",
+		    ";":"",
+		    ":":"",
+		    ")":"",
+		    "(":"",
+		    "'":"",
+
+		    '"':"",
+		    "1":"",
+		    "2":"",
+		    "3":"",
+		    "4":"",
+		    "5":"",
+		    "6":"",
+		    "7":"",
+		    "8":"",
+		    "9":"",
+		    "0":"",
+		    "-":"",
+		    "_":"",
+		    "?":"",
+		    "%":"",
+		    "$":"",
+		    "&":"",
+		    "/":"",
+		    "\\":""} #This set is not complete!!
+		self.n = ngram.ngram()
+		self.feedList = []
+	def add_rss(self,fileName):
+		self.feedList.append(fileName)
+	#Replace language specfiic characters
+	def repChar(self,inputStr):
+	    ret = inputStr.encode("utf-8")
+	    for x in self.charList:
+		ret = ret.replace(x,self.charList[x])
+	    return ret
+	
+	def process(self):
+		print self.feedList
+		for fd in self.feedList:
+			feed = getFeed(fd)		
+			for channel in feed:
+				print "Feed: %s, %s" %(channel["title"],channel["desc"])
+				print "Language: %s" %(channel["lang"])
+				for obj in channel["titles"]:
+					for x in self.repChar(obj["title"]).lower().split():
+						self.n.update(x,channel["lang"])
+					for x in self.repChar(obj["desc"]).lower().split():
+						self.n.update(x,channel["lang"])                
+	def getNgram(self):
+		return self.n
